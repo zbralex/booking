76134e68ad9aea16aeef09cf5e187aedc6a58a75
Александр Зубрицкий <a.zubritsky@ursgis.ru>
2021-08-27 15:23:13 +0300

    исправляет событие при наведении

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,44 +1,53 @@
-import { Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MapSetService} from '../../services/map-set.service';
 
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+    selector: 'app-map',
+    templateUrl: './map.component.html',
+    styleUrls: ['./map.component.scss']
 })
 
 export class MapComponent implements OnInit {
-  center: any = [55.771209, 37.568156];
-  placemarks: PlacemarkConstructor[] = [];
-  zoom = 10;
+    center: any = [55.771209, 37.568156];
+    placemarks: PlacemarkConstructor[] = [];
+    zoom = 10;
+    options: ymaps.IPlacemarkOptions = {
+        preset: 'islands#redStretchyIcon'
+    };
+    // создается второй объект настроек свойства preset по умолчанию и задается, когда мышь уходит
+    unsetOptions: ymaps.IPlacemarkOptions = {
+        preset: 'islands#darkBlueStretchyIcon'
+    };
 
 
-  constructor(private mapSetService: MapSetService) {
-  }
+    constructor(private mapSetService: MapSetService) {
+    }
 
-  ngOnInit(): void {
-   this.mapSetService.passPreparedData
-       .subscribe(preparedItems => {
-      this.placemarks = preparedItems;
-    });
+    ngOnInit(): void {
+        this.mapSetService.passPreparedData
+            .subscribe(preparedItems => {
+                this.placemarks = preparedItems;
+            });
 
-    this.mapSetService.eventAndIndex.subscribe((res) => {
-      const ind = res.index;
-      if(res.e.type === 'mouseenter') {
-        this.placemarks[ind].options.preset = 'islands#redStretchyIcon';
-        const newEl = this.placemarks[ind];
-        this.placemarks.push(newEl);
-      } else {
-        this.placemarks.pop()
-      }
-    })
+        this.mapSetService.eventAndIndex.subscribe((res) => {
+            const ind = res.index;
+            this.setColorElement(ind, res.e.type);
+        });
 
-  }
+    }
 
+    setColorElement(index, type) {
+        this.placemarks.forEach((item, i) => {
+            if (index === i) {
+                item.options = type === 'mouseenter' ? this.options : this.unsetOptions;
+            }
+        });
+    }
 }
+
 interface PlacemarkConstructor {
-  geometry: number[];
-  properties: ymaps.IPlacemarkProperties;
-  options: ymaps.IPlacemarkOptions;
+    geometry: number[];
+    properties: ymaps.IPlacemarkProperties;
+    options: ymaps.IPlacemarkOptions;
 }
