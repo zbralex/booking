266280f0625fce8c372252ab1a4cac2331686be4
Александр Зубрицкий <a.zubritsky@ursgis.ru>
2021-09-02 14:36:58 +0300

    блокирует кнопки перелистывания месяца

--- src/app/pages/room/room-detail/room-detail.component.ts
+++ src/app/pages/room/room-detail/room-detail.component.ts
@@ -20,16 +20,27 @@ export class RoomDetailComponent implements OnInit {
     @ViewChild('calendarLeft', {static: false}) calendarLeft: MatCalendar<Date>;
     @ViewChild('calendarRight', {static: false}) calendarRight: MatCalendar<Date>;
     headerComponent = CustomCalendarHeaderComponent;
+    minDateToChoice: Date = new Date();
+    disabledMinDate: boolean = true;
+
+    disCounter: number = new Date().getMonth();
 
 
-
-
-    constructor(private matDialog: MatDialog, ) {
+    constructor(private matDialog: MatDialog,) {
 
     }
 
 
     ngOnInit(): void {
+    }
+
+    disabledButtonPrev() {
+        if (this.minDateToChoice.getMonth() === this.disCounter) {
+            return true;
+        }
+        if (this.disCounter > this.minDateToChoice.getMonth()) {
+            return false;
+        }
     }
 
 
@@ -45,10 +56,10 @@ export class RoomDetailComponent implements OnInit {
                 date
             );
             this.selected = this.selectedDateRange;
-            console.log(this.selectedDateRange, 'if');
+            console.log(this.selectedDateRange, 'all');
         } else {
             this.selectedDateRange = new DateRange(date, null);
-            console.log(this.selectedDateRange, 'else');
+            console.log(this.selectedDateRange, 'one');
         }
     }
 
@@ -66,19 +77,19 @@ export class RoomDetailComponent implements OnInit {
     }
 
     nextMonth() {
-       this.startDate = new Date(this.startDate.getFullYear(), this.startDate.getMonth() + 1, 1);
-        // console.log(this.startDate);
-        this.calendarLeft._goToDateInView(this.startDate, 'month')
-        this.calendarRight._goToDateInView(new Date(this.startDate.getFullYear(), this.startDate.getMonth() + 1), "month")
-       this.clearDates()
+        this.disCounter++;
+        this.startDate = new Date(this.startDate.getFullYear(), this.startDate.getMonth() + 1, 1);
+        this.calendarLeft._goToDateInView(this.startDate, 'month');
+        this.calendarRight._goToDateInView(new Date(this.startDate.getFullYear(), this.startDate.getMonth() + 1), 'month');
+        this.clearDates();
     }
 
     prevMonth() {
+        this.disCounter--;
         this.startDate = new Date(this.startDate.getFullYear(), this.startDate.getMonth() - 1, 1);
-
-        this.calendarLeft._goToDateInView(this.startDate, 'month')
-        this.calendarRight._goToDateInView(new Date(this.startDate.getFullYear(), this.startDate.getMonth() - 1), "month")
-        this.clearDates()
+        this.calendarLeft._goToDateInView(this.startDate, 'month');
+        this.calendarRight._goToDateInView(new Date(this.startDate.getFullYear(), this.startDate.getMonth() - 1), 'month');
+        this.clearDates();
     }
 }
 
