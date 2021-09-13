9d9ad675e9526a77258fa1036bb26fc6920fe771
Александр Зубрицкий <a.zubritsky@ursgis.ru>
2021-09-13 15:53:35 +0300

    исправляет список избранных

--- src/app/pages/room/room-detail/room-detail.component.ts
+++ src/app/pages/room/room-detail/room-detail.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, OnInit, ViewChild} from '@angular/core';
+import {AfterContentChecked, ChangeDetectionStrategy, Component, OnInit, ViewChild} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import {DateRange, MatCalendar} from '@angular/material/datepicker';
 import {CustomCalendarHeaderComponent} from '../components/custom-calendar-header/custom-calendar-header.component';
@@ -14,7 +14,7 @@ import {tap} from 'rxjs/operators';
     styleUrls: ['./room-detail.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RoomDetailComponent implements OnInit {
+export class RoomDetailComponent implements OnInit, AfterContentChecked {
     images: any;
     selected: DateRange<Date>;
 
@@ -32,21 +32,17 @@ export class RoomDetailComponent implements OnInit {
     favorites$: Observable<any> = new Observable<any>();
     favorites: any = localStorage.getItem('favorites');
     favoritesTest: any = localStorage.getItem('favorites');
-    disableButton$: Observable<any>;
-
-
     disCounter: number = new Date().getMonth();
 
     dates: any = {
         start: '',
         end: ''
     };
-    test$: Observable<any>;
-    test: boolean;
+
     subj: Subject<any> = new Subject<any>();
     deleteItem: number;
     addItem: number;
-    s: Subject<any> = new Subject<any>();
+    disableButton$: Subject<any> = new Subject<any>();
     disable: boolean;
 
     constructor(private matDialog: MatDialog,
@@ -57,38 +53,31 @@ export class RoomDetailComponent implements OnInit {
 
     }
 
-    // disableButtonFavorites(): void {
-    //
-    //     if (!this.favorites) {
-    //         this.disableButton$ = new Observable<any>(obs => {
-    //             obs.next(false);
-    //         });
-    //     } else {
-    //         const arrayFavorites = JSON.parse(this.favorites);
-    //         const findAdded = arrayFavorites.find((item) => item.id === +this.route.snapshot.params.id);
-    //         // console.log(findAdded, 'findAdded');
-    //         this.disableButton$ = new Observable<any>(obs => {
-    //             if (findAdded) {
-    //                 obs.next(true);
-    //                 this.i();
-    //             } else {
-    //                 this.d();
-    //             }
-    //         });
-    //     }
-    // }
-
-    ngOnInit(): void {
-        this.getDatesFromUrl();
-        this.subscribeOnFavorites();
-        this.s.subscribe(res => {
-            this.disable = res;
-        });
-        this.getFavorites().subscribe();
-
+    ngAfterContentChecked(): void {
+        this.disableButtonFavorites();
     }
 
-// =========================================================================== new code
+    disableButtonFavorites(): void {
+        if (!this.favorites) {
+            this.disableButton$.next(true);
+        } else {
+            const arrayFavorites = JSON.parse(this.favorites);
+            const findAdded = arrayFavorites.find((item) => item.id === +this.route.snapshot.params.id);
+
+            if (findAdded) {
+                this.disableButton$.next(true);
+            }
+        }
+    }
+
+    ngOnInit(): void {
+        this.disableButtonFavorites();
+        this.getDatesFromUrl();
+        this.subscribeOnFavorites();
+        this.getFavorites().subscribe();
+    }
+
+
     addFavorite(): void {
         if (!this.favorites) {
             localStorage.setItem('favorites', JSON.stringify([{id: +this.route.snapshot.params.id}]));
@@ -97,17 +86,8 @@ export class RoomDetailComponent implements OnInit {
             this.subj.next(this.favoritesTest);
             localStorage.setItem('favorites', JSON.stringify(this.favoritesTest));
             this.setFavoriteSubj.subscribe((res) => {
-                // console.log(res, 'add fav');
-                this.addItem = res.id;
-                // console.log(this.addItem);
-                this.s.next(true);
+                this.disableButton$.next(true);
                 this.favoritesService.setFavorite(res);
-
-                // this.disableButton$ = new Observable<any>(r => {
-                //     if (this.addItem) {
-                //         r.next(true);
-                //     }
-                // });
             });
             this.setFavoriteSubj.next({id: +this.route.snapshot.params.id});
         }
@@ -129,23 +109,8 @@ export class RoomDetailComponent implements OnInit {
         }));
     }
 
-// =========================================================================== new code end
-    d(): void {
-        this.test$ = new Observable<any>(res => {
-            res.next(false);
-        });
-    }
-
-    i(): void {
-        this.test$ = new Observable<any>(res => {
-            res.next(true);
-        });
-    }
 
     subscribeOnFavorites(): void {
-        // this.disableButton$ = new Observable<any>(t => {
-        //     t.next(false);
-        // });
         const arrayFavorites = JSON.parse(this.favorites);
         // создаем поток из элементов localstorage
         this.favorites$ = new Observable<any>(obs => {
@@ -156,12 +121,8 @@ export class RoomDetailComponent implements OnInit {
         });
         // подписываемся на события из сервиса, который передает элементы из room-detail в компонент favorites
         this.favoritesService.unsetEmit.subscribe(res => {
-            // TODO: найти способ включать кнопку обратно
-            this.deleteItem = res.id;
-            // console.log(res, this.deleteItem, 'res ++++');
             this.removeFromFavorites(res.id);
-            this.s.next(false);
-            // console.log(this.disable, 'disable');
+            this.disableButton$.next(false);
         });
 
     }
@@ -224,31 +185,6 @@ export class RoomDetailComponent implements OnInit {
     openModalReview($event): void {
         console.log($event);
     }
-
-    // setFavorites(): void {
-    //     if (!this.favorites) {
-    //         localStorage.setItem('favorites', JSON.stringify([{id: +this.route.snapshot.params.id}]));
-    //     } else {
-    //         const arrayFavorites = JSON.parse(this.favorites);
-    //         const findAdded = arrayFavorites.find((item) => item.id === +this.route.snapshot.params.id);
-    //
-    //         if (findAdded) {
-    //             this.snackBar.open('Это предложение уже добавлено в избранное', '', {
-    //                     duration: 3000,
-    //                 }
-    //             );
-    //         } else {
-    //             // добавляем
-    //             arrayFavorites.push({id: +this.route.snapshot.params.id});
-    //             localStorage.setItem('favorites', JSON.stringify(arrayFavorites));
-    //             this.setFavoriteSubj.subscribe((res) => {
-    //                 this.favoritesService.setFavorite(res);
-    //             });
-    //             this.setFavoriteSubj.next({id: +this.route.snapshot.params.id});
-    //             this.i();
-    //         }
-    //     }
-    // }
 
     clearDates(): void {
         this.selected = null;
